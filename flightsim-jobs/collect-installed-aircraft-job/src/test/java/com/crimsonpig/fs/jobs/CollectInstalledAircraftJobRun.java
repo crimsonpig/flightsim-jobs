377dadd885f82f25d17fe354d1679d84e22af384
crimsonpig <keith.stabbert@yahoo.com>
2016-02-09 22:30:14 -0800

    Adding collect-installed-aircraft-job

--- flightsim-jobs/collect-installed-aircraft-job/src/test/java/com/crimsonpig/fs/jobs/CollectInstalledAircraftJobRun.java
+++ flightsim-jobs/collect-installed-aircraft-job/src/test/java/com/crimsonpig/fs/jobs/CollectInstalledAircraftJobRun.java
@@ -5,7 +5,7 @@ import org.springframework.batch.core.launch.support.CommandLineJobRunner;
 public class CollectInstalledAircraftJobRun {
 
 	public static void main(String[] args) throws Exception{
-		String args2[] = {"/jobs/CollectInstalledAircraftJob.xml", "CollectInstalledAircraftJob"};
+		String args2[] = {"/CollectInstalledAircraftJob.xml", "CollectInstalledAircraftJob"};
 		CommandLineJobRunner.main(args2);
 	}
 }
