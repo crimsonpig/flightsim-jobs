d43f49629f29d52aa61a50b3c91a36b12d7ba709
crimsonpig <keith.stabbert@yahoo.com>
2016-02-09 22:35:21 -0800

    Creating flightplan-aircraft-job

--- flightsim-jobs/flightplan-aircraft-job/src/test/java/com/crimsonpig/fs/jobs/FlightplanAircraftJobRun.java
+++ flightsim-jobs/flightplan-aircraft-job/src/test/java/com/crimsonpig/fs/jobs/FlightplanAircraftJobRun.java
@@ -5,7 +5,7 @@ import org.springframework.batch.core.launch.support.CommandLineJobRunner;
 public class FlightplanAircraftJobRun {
 	
 	public static void main(String[] args) throws Exception{
-		String args2[] = {"/jobs/FlightplanAircraftJob.xml", "FlightplanAircraftJob"};
+		String args2[] = {"/FlightplanAircraftJob.xml", "FlightplanAircraftJob"};
 		CommandLineJobRunner.main(args2);
 	}
 }
