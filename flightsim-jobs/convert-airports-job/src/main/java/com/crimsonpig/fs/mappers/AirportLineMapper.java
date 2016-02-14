95e2e3996437a0067d0954174e49446794b11b37
crimsonpig <keith.stabbert@yahoo.com>
2016-02-14 00:14:38 -0800

    Cleaning up AirportLineMapper

--- flightsim-jobs/convert-airports-job/src/main/java/com/crimsonpig/fs/mappers/AirportLineMapper.java
+++ flightsim-jobs/convert-airports-job/src/main/java/com/crimsonpig/fs/mappers/AirportLineMapper.java
@@ -1,14 +1,30 @@
 package com.crimsonpig.fs.mappers;
 
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.file.transform.LineTokenizer;
 
 import com.crimsonpig.fs.domain.airport.Airport;
 
 public class AirportLineMapper extends DefaultLineMapper<Airport> {
 
 	public AirportLineMapper(){
-		setLineTokenizer(new AirportLineTokenizer());
-		setFieldSetMapper(new AirportFieldSetMapper());
+		setLineTokenizer(airportLineTokenizer());
+		setFieldSetMapper(airportFieldSetMapper());
+	}
+	
+	private LineTokenizer airportLineTokenizer(){
+		DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+		tokenizer.setDelimiter(",");
+		tokenizer.setNames(new String[]{"identifier","latitude","longitude","elevation"});
+		return tokenizer;
 	}
 
+	private FieldSetMapper<Airport> airportFieldSetMapper(){
+		BeanWrapperFieldSetMapper<Airport> wrapper = new BeanWrapperFieldSetMapper<Airport>();
+		wrapper.setTargetType(Airport.class);
+		return wrapper;
+	}
 }
