d0f258be23bc756ae36b77aeb43e1357b0d6dc20
crimsonpig <keith.stabbert@yahoo.com>
2016-02-13 23:53:07 -0800

    Adding JobComponentsConfig

---
+++ flightsim-jobs/convert-airports-job/src/main/java/com/crimsonpig/fs/config/JobComponentsConfig.java
@@ -0,0 +1,60 @@
+package com.crimsonpig.fs.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import com.crimsonpig.fs.domain.airport.Airport;
+import com.crimsonpig.fs.domain.airport.ConvertedAirport;
+import com.crimsonpig.fs.mappers.AirportLineMapper;
+import com.crimsonpig.fs.processors.AirportProcessor;
+import com.crimsonpig.fs.tasklet.TruncateTableTasklet;
+
+@Configuration
+public class JobComponentsConfig {
+
+	@Autowired
+	private DataSource domainDataSource;
+
+	@Bean(name = "truncateTableTasklet")
+	public Tasklet tasklet() {
+		TruncateTableTasklet tasklet = new TruncateTableTasklet();
+		tasklet.setTable("CONVERTEDAIRPORTS");
+		tasklet.setDataSource(domainDataSource);
+		return tasklet;
+	}
+
+	@Bean(name = "airportsFileReader")
+	public ItemReader<Airport> reader() {
+		FlatFileItemReader<Airport> reader = new FlatFileItemReader<Airport>();
+		reader.setResource(new ClassPathResource("data/AirportsFSX1.txt"));
+		reader.setLineMapper(new AirportLineMapper());
+		return reader;
+	}
+
+	@Bean(name = "airportLatLonDecimalConverter")
+	public ItemProcessor<Airport,ConvertedAirport> processor(){
+		return new AirportProcessor();
+	}
+
+	@Bean(name = "convertedAirportsWriter")
+	public ItemWriter<ConvertedAirport> writer() {
+		JdbcBatchItemWriter<ConvertedAirport> writer = new JdbcBatchItemWriter<ConvertedAirport>();
+		writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<ConvertedAirport>());
+		writer.setAssertUpdates(true);
+		writer.setDataSource(domainDataSource);
+		writer.setSql("INSERT INTO CONVERTEDAIRPORTS(IDENT,LATITUDE,LONGITUDE,ELEVATION) VALUES(:identifier, :latitudeRadians, :longitudeRadians, :elevation)");
+		return writer;
+	}
+
+}
