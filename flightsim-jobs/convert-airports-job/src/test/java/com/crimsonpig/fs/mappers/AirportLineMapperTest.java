f65b173bb7cd6ee92aa8d0f6224cd407556fff1b
crimsonpig <keith.stabbert@yahoo.com>
2016-02-14 00:19:13 -0800

    Adding AirportLineMapperTest

---
+++ flightsim-jobs/convert-airports-job/src/test/java/com/crimsonpig/fs/mappers/AirportLineMapperTest.java
@@ -0,0 +1,28 @@
+package com.crimsonpig.fs.mappers;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.springframework.batch.item.file.LineMapper;
+
+import com.crimsonpig.fs.domain.airport.Airport;
+
+public class AirportLineMapperTest {
+
+	@Test
+	public void mapLineToAirport(){
+		LineMapper<Airport> mapper = new AirportLineMapper();
+		String lineToMap = "KSEA,N47* 26.94000',W122* 18.55830',433";
+		Airport ksea = null;
+		try {
+			ksea = mapper.mapLine(lineToMap, 1);
+		} catch (Exception e) {
+			fail();
+		}
+		assertEquals("KSEA",ksea.getIdentifier());
+		assertEquals("N47* 26.94000'", ksea.getLatitude());
+		assertEquals("W122* 18.55830'", ksea.getLongitude());
+		assertEquals(433, ksea.getElevation());
+	}
+}
