1bb7ce174ff866935237c7da865aabd9c11c1840
crimsonpig <keith.stabbert@yahoo.com>
2016-02-23 22:22:49 -0800

    Adding FullRouteDefinitionLineMapper

--- flightsim-jobs/full-route-definitions-job/src/test/java/com/crimsonpig/fs/mappers/RouteDefinitionMapperTest.java
+++ flightsim-jobs/full-route-definitions-job/src/test/java/com/crimsonpig/fs/mappers/RouteDefinitionMapperTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.fail;
 import org.junit.Test;
 import org.springframework.batch.item.file.LineMapper;
 
+import com.crimsonpig.fs.domain.route.FullRouteDefinition;
 import com.crimsonpig.fs.domain.route.SimpleRouteDefinition;
 
 public class RouteDefinitionMapperTest {
@@ -26,6 +27,34 @@ public class RouteDefinitionMapperTest {
 		assertEquals("KSMF", route.getOriginAirport());
 		assertEquals("KLAX", route.getDestinationAirport());
 		assertEquals(6, route.getFlightFrequency());
+		assertEquals(370, route.getLowestFlightLevel());
+		
+	}
+	
+	@Test
+	public void mapLineToFullRouteDefinition(){
+		
+		LineMapper<FullRouteDefinition> mapper = new FullRouteDefinitionLineMapper();
+		String lineToMap = "Airwave,MD80,5,McDonnell-Douglas/Boeing MD-83 Paint1,437,-7,KPDX,0.79567329,-2.13973003,KSMF,0.67536243,-2.12215942,5,370";
+		FullRouteDefinition route = null;
+		try {
+			route = mapper.mapLine(lineToMap, 0);
+		} catch (Exception e){
+			fail();
+		}
+		assertEquals("Airwave", route.getAirline());
+		assertEquals("MD80", route.getAircraftModel());
+		assertEquals(5, route.getAircraftNumber());
+		assertEquals("McDonnell-Douglas/Boeing MD-83 Paint1", route.getAircraftTitle());
+		assertEquals(437, route.getGroundspeed());
+		assertEquals(-7, route.getOriginTimezone());
+		assertEquals("KPDX", route.getOriginAirport());
+		assertEquals(0.79567329, route.getOriginLatitude(), 0);
+		assertEquals(-2.13973003, route.getOriginLongitude(), 0);
+		assertEquals("KSMF", route.getDestinationAirport());
+		assertEquals(0.67536243, route.getDestinationLatitude(), 0);
+		assertEquals(-2.12215942, route.getDestinationLongitude(), 0);
+		assertEquals(5, route.getFlightFrequency());
 		assertEquals(370, route.getLowestFlightLevel());
 		
 	}
