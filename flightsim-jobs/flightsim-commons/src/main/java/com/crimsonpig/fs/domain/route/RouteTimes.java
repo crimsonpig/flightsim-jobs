f6e66ff471b73ac7d73a33629c917d4a023a15b5
crimsonpig <keith.stabbert@yahoo.com>
2016-02-23 22:08:59 -0800

    Simplifying RouteTimes

--- flightsim-jobs/flightsim-commons/src/main/java/com/crimsonpig/fs/domain/route/RouteTimes.java
+++ flightsim-jobs/flightsim-commons/src/main/java/com/crimsonpig/fs/domain/route/RouteTimes.java
@@ -3,42 +3,30 @@ package com.crimsonpig.fs.domain.route;
 
 public class RouteTimes {
 	
-	private long minimumHoldTimeInSeconds;
+	private long holdTime;
 	private long routeLegSeconds;
-	private long minimumLegSeconds;
-	private long singleRepetitionSeconds;
-
-	public long getMinimumHoldTimeInSeconds() {
-		return minimumHoldTimeInSeconds;
+	
+	public long getHoldTime() {
+		return holdTime;
 	}
-
-	public void setMinimumHoldTimeInSeconds(long minimumHoldTimeInSeconds) {
-		this.minimumHoldTimeInSeconds = minimumHoldTimeInSeconds;
+	
+	public void setHoldTime(long holdTime) {
+		this.holdTime = holdTime;
 	}
-
+	
 	public long getRouteLegSeconds() {
 		return routeLegSeconds;
 	}
-
+	
 	public void setRouteLegSeconds(long routeLegSeconds) {
 		this.routeLegSeconds = routeLegSeconds;
 	}
 
 	public long getMinimumLegSeconds() {
-		return minimumLegSeconds;
-	}
-
-	public void setMinimumLegSeconds(long minimumLegSeconds) {
-		this.minimumLegSeconds = minimumLegSeconds;
+		return holdTime + getRouteLegSeconds();
 	}
 
 	public long getSingleRepetitionSeconds() {
-		return singleRepetitionSeconds;
+		return 2 * getMinimumLegSeconds();
 	}
-
-	public void setSingleRepetitionSeconds(long singleRepetitionSeconds) {
-		this.singleRepetitionSeconds = singleRepetitionSeconds;
-	}
-	
-
 }
