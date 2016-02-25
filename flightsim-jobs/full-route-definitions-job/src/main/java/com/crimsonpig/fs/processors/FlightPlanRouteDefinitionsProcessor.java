6b8aaa1381d187307a175232feb82515c72d86f9
crimsonpig <keith.stabbert@yahoo.com>
2016-02-24 21:27:10 -0800

    Adding DistanceAndHeadingService to processor

--- flightsim-jobs/full-route-definitions-job/src/main/java/com/crimsonpig/fs/processors/FlightPlanRouteDefinitionsProcessor.java
+++ flightsim-jobs/full-route-definitions-job/src/main/java/com/crimsonpig/fs/processors/FlightPlanRouteDefinitionsProcessor.java
@@ -4,9 +4,16 @@ import org.springframework.batch.item.ItemProcessor;
 
 import com.crimsonpig.fs.domain.route.FlightPlanRouteDefinition;
 import com.crimsonpig.fs.domain.route.FullRouteDefinition;
+import com.crimsonpig.fs.service.DistanceAndHeadingService;
 
 public class FlightPlanRouteDefinitionsProcessor implements
 		ItemProcessor<FullRouteDefinition, FlightPlanRouteDefinition> {
+
+	private DistanceAndHeadingService distanceAndHeading;
+
+	public void setDistanceAndHeading(DistanceAndHeadingService distanceAndHeading) {
+		this.distanceAndHeading = distanceAndHeading;
+	}
 
 	@Override
 	public FlightPlanRouteDefinition process(FullRouteDefinition item)
