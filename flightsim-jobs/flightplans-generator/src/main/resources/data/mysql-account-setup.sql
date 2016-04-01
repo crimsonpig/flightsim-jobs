ad195d90f22a88abf00f27900a17a9db08b83760
crimsonpig <keith.stabbert@yahoo.com>
2016-04-01 14:35:26 -0700

    Cleaning up old flightplans generator job

--- flightsim-jobs/flightplans-generator/src/main/resources/data/mysql-account-setup.sql
+++
@@ -1,6 +0,0 @@
-DROP USER 'fsmanager'@'localhost';
-DROP USER 'fsmanager'@'%';
-CREATE USER 'fsmanager'@'localhost' IDENTIFIED BY 'fman';
-CREATE USER 'fsmanager'@'%' IDENTIFIED BY 'fman';
-GRANT INSERT, SELECT, UPDATE, DELETE, CREATE, DROP ON FSTOOLS.* TO 'fsmanager'@'%' IDENTIFIED BY 'fman';
-GRANT INSERT, SELECT, UPDATE, DELETE, CREATE, DROP ON FSTOOLS.* TO 'fsmanager'@'%' IDENTIFIED BY 'fman';
