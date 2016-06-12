5377da3b070331558c4683c985495f011cd75439
crimsonpig <keith.stabbert@yahoo.com>
2016-06-11 17:19:59 -0700

    Completing FullRouteDefinitionsProcessorTest

--- flightsim-jobs/generate-flightplans-job/src/test/java/com/crimsonpig/fs/processors/FullRouteDefinitionsProcessorTest.java
+++ flightsim-jobs/generate-flightplans-job/src/test/java/com/crimsonpig/fs/processors/FullRouteDefinitionsProcessorTest.java
@@ -1,5 +1,6 @@
 package com.crimsonpig.fs.processors;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import org.junit.Before;
@@ -22,8 +23,8 @@ public class FullRouteDefinitionsProcessorTest {
 		
 		RetrieveAirportService airportService = new RetrieveAirportService();
 		StubConvertedAirportReader airportReader = new StubConvertedAirportReader();
-		airportReader.addAirportToRetrieve(ConvertedAirportStubs.getSmfAirport());
 		airportReader.addAirportToRetrieve(ConvertedAirportStubs.getDenAirport());
+		airportReader.addAirportToRetrieve(ConvertedAirportStubs.getSmfAirport());
 		airportService.setDatabaseReader(airportReader);
 		processor.setAirportService(airportService);
 		
@@ -38,11 +39,26 @@ public class FullRouteDefinitionsProcessorTest {
 	public void testProcessRouteDefinition(){
 		
 		SimpleRouteDefinition simpleRoute = SimpleRouteDefinitionStubs.getSmfToDen();
+		FullRouteDefinition fullRoute = null;
 		try {
-			FullRouteDefinition fullRoute = processor.process(simpleRoute);
+			fullRoute = processor.process(simpleRoute);
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail();
 		}
+		assertEquals("B734", fullRoute.getAircraftModel());
+		assertEquals("Boeing 737-400 Paint1", fullRoute.getAircraftTitle());
+		assertEquals("American Pacific", fullRoute.getAirline());
+		assertEquals(1, fullRoute.getAircraftNumber());
+		assertEquals(430, fullRoute.getGroundspeed());
+		assertEquals(8, fullRoute.getFlightFrequency());
+		assertEquals("KSMF", fullRoute.getOriginAirport());
+		assertEquals(0.67536243, fullRoute.getOriginLatitude(), 0.00000000001);
+		assertEquals(-2.12215942, fullRoute.getOriginLongitude(), 0.00000000001);
+		assertEquals(-7, fullRoute.getOriginTimezone());
+		assertEquals("KDEN", fullRoute.getDestinationAirport());
+		assertEquals(0.69571713, fullRoute.getDestinationLatitude(), 0.00000000001);
+		assertEquals(-1.82689160, fullRoute.getDestinationLongitude(), 0.00000000001);
+
 	}
 }
--- flightsim-jobs/generate-flightplans-job/src/test/java/com/crimsonpig/fs/stubs/readers/StubConvertedAirportReader.java
+++ flightsim-jobs/generate-flightplans-job/src/test/java/com/crimsonpig/fs/stubs/readers/StubConvertedAirportReader.java
@@ -2,6 +2,7 @@ package com.crimsonpig.fs.stubs.readers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.crimsonpig.fs.config.readers.ConvertedAirportReader;
 import com.crimsonpig.fs.domain.airport.ConvertedAirport;
@@ -11,7 +12,10 @@ public class StubConvertedAirportReader extends ConvertedAirportReader {
 	private List<ConvertedAirport> foundAirports = new ArrayList<ConvertedAirport>();
 	
 	public List<ConvertedAirport> retrieveAirport(String identifier){
-		return foundAirports;	
+		return foundAirports
+				.stream()
+				.filter(airport -> airport.getIdentifier().equals(identifier))
+				.collect(Collectors.toList());	
 	}
 	
 	public void addAirportToRetrieve(ConvertedAirport airport){
