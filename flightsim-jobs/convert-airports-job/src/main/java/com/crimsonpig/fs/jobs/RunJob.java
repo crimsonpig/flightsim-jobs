5c7340956eb01465c92201ac5e0bde7b9fcfbcb3
crimsonpig <keith.stabbert@yahoo.com>
2016-07-05 18:16:08 -0700

    Creating new standard for job runner class

--- flightsim-jobs/convert-airports-job/src/main/java/com/crimsonpig/fs/jobs/RunJob.java
+++ flightsim-jobs/convert-airports-job/src/main/java/com/crimsonpig/fs/jobs/RunJob.java
@@ -2,7 +2,7 @@ package com.crimsonpig.fs.jobs;
 
 import org.springframework.batch.core.launch.support.CommandLineJobRunner;
 
-public class ConvertAirportsJobRun {
+public class RunJob {
 
 	public static void main(String[] args) throws Exception{
 		String args2[] = {"/ConvertAirportsJob.xml", "ConvertAirportsJob"};
