b42f2012e98871e0b164db52c5a340f99909da9f
crimsonpig <keith.stabbert@yahoo.com>
2016-07-31 01:11:14 -0700

    Finishing InstalledAircraftConfig

---
+++ flightsim-jobs/collect-installed-aircraft-job/src/main/java/com/crimsonpig/fs/config/InstalledAircraftConfig.java
@@ -0,0 +1,62 @@
+package com.crimsonpig.fs.config;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.crimsonpig.fs.domain.aircraft.InstalledAircraft;
+import com.crimsonpig.fs.domain.aircraft.config.AircraftConfigFile;
+import com.crimsonpig.fs.processors.InstalledAircraftProcessor;
+import com.crimsonpig.fs.tasklet.TruncateTableTasklet;
+import com.crimsonpig.fs.writers.ListItemWriter;
+
+@Configuration
+public class InstalledAircraftConfig {
+
+	@Autowired
+	private DataSource domainDataSource;
+	
+	private static final String INSERT_INSTALLED_AIRCRAFT = "INSERT INTO INSTALLEDAIRCRAFT("
+			+ "TITLE, AIRCRAFT_FOLDER, AIRLINE, ATC_TYPE,ATC_MODEL,UI_MANUFACTURER,UI_TYPE,UI_VARIATION)"
+			+ "VALUES(:title,:aircraftFolder,:airline,:atcType,:atcModel,:uiManufacturer,:uiType,:uiVariation)";
+	
+	@Bean(name="deleteInstalledAircraftTasklet")
+	public Tasklet deleteInstalledAircraftTasklet(){
+		TruncateTableTasklet tasklet = new TruncateTableTasklet();
+		tasklet.setDataSource(domainDataSource);
+		tasklet.setTable("INSTALLEDAIRCRAFT");
+		return tasklet;
+	}
+	
+	@Bean(name="installedAircraftProcessor")
+	public ItemProcessor<AircraftConfigFile, List<InstalledAircraft>> installedAircraftProcessor(){
+		return new InstalledAircraftProcessor();
+	}
+	
+	@Bean(name="installedAircraftListWriter")
+	public ItemWriter<List<InstalledAircraft>> installedAircraftListWriter(){
+		ListItemWriter<InstalledAircraft> listWriter = new ListItemWriter<InstalledAircraft>();
+		listWriter.setItemWriter(installedAircraftWriter());
+		return listWriter;
+	}
+	
+	private ItemWriter<InstalledAircraft> installedAircraftWriter(){
+		JdbcBatchItemWriter<InstalledAircraft> jdbcWriter = new JdbcBatchItemWriter<InstalledAircraft>();
+		jdbcWriter.setDataSource(domainDataSource);
+		jdbcWriter.setAssertUpdates(true);
+		jdbcWriter.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<InstalledAircraft>());
+		jdbcWriter.setSql(INSERT_INSTALLED_AIRCRAFT);
+		return jdbcWriter;
+	}
+	
+	
+}
