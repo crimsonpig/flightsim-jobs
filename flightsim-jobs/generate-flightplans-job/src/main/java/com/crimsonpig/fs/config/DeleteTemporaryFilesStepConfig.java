3817b4b88af2f34d6b7cf396dd10b93dd7e7b5aa
crimsonpig <keith.stabbert@yahoo.com>
2016-08-06 21:32:30 -0700

    Adding step to generate-flightplans-job to delete temporary files at the end

---
+++ flightsim-jobs/generate-flightplans-job/src/main/java/com/crimsonpig/fs/config/DeleteTemporaryFilesStepConfig.java
@@ -0,0 +1,32 @@
+package com.crimsonpig.fs.config;
+
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+
+import com.crimsonpig.fs.tasklet.DeleteFilesTasklet;
+
+public class DeleteTemporaryFilesStepConfig {
+
+	@Autowired
+	private BatchPropertiesConfig batchProperties;
+	
+	
+	@Bean(name = "deleteTempFilesTasklet")
+	public Tasklet deleteTemporaryFilesTasklet() throws MalformedURLException{
+		DeleteFilesTasklet tasklet = new DeleteFilesTasklet();
+		List<Resource> filesToDelete = new ArrayList<Resource>(3);
+		filesToDelete.add(new UrlResource(batchProperties.getProperty("full.routes.file")));
+		filesToDelete.add(new UrlResource(batchProperties.getProperty("flightplan.routes.file")));
+		filesToDelete.add(new UrlResource(batchProperties.getProperty("flightplan.aircraft.file")));
+		return tasklet;
+		
+	}
+	
+}
