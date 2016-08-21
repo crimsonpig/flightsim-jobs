8f801fc0d2e5b095b459cc425fdaddb5f22d1ec8
crimsonpig <keith.stabbert@yahoo.com>
2016-08-21 15:11:13 -0700

    Adding airport bo

---
+++ flightsim-jobs/flightsim-commons/src/main/java/com/crimsonpig/fs/domain/airport/Airport.java
@@ -0,0 +1,83 @@
+package com.crimsonpig.fs.domain.airport;
+
+public class Airport {
+
+	private String identifier;
+	private double latitudeDegrees;
+	private double longitudeDegrees;
+	private double altitude;
+	private String airspace;
+	private int approachFrequencies;
+	private double trafficScalar;
+	private String country;
+	
+	public Airport() {
+
+	}
+
+	public String getIdentifier() {
+		return identifier;
+	}
+
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}
+
+	public double getLatitudeDegrees() {
+		return latitudeDegrees;
+	}
+
+	public void setLatitudeDegrees(double latitudeDegrees) {
+		this.latitudeDegrees = latitudeDegrees;
+	}
+
+	public double getLongitudeDegrees() {
+		return longitudeDegrees;
+	}
+
+	public void setLongitudeDegrees(double longitudeDegrees) {
+		this.longitudeDegrees = longitudeDegrees;
+	}
+
+	public double getAltitude() {
+		return altitude;
+	}
+
+	public void setAltitude(double altitude) {
+		this.altitude = altitude;
+	}
+
+	public String getAirspace() {
+		return airspace;
+	}
+
+	public void setAirspace(String airspace) {
+		this.airspace = airspace;
+	}
+
+	public int getApproachFrequencies() {
+		return approachFrequencies;
+	}
+
+	public void setApproachFrequencies(int approachFrequencies) {
+		this.approachFrequencies = approachFrequencies;
+	}
+
+	public double getTrafficScalar() {
+		return trafficScalar;
+	}
+
+	public void setTrafficScalar(double trafficScalar) {
+		this.trafficScalar = trafficScalar;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	
+}
