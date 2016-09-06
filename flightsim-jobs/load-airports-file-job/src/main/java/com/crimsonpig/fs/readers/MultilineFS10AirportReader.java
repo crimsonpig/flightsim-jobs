c05e41db2dcd1fe50b83cf259a25de8fbf21305e
crimsonpig <keith.stabbert@yahoo.com>
2016-09-05 21:30:06 -0700

    Making field set mappers instance variables.

--- flightsim-jobs/load-airports-file-job/src/main/java/com/crimsonpig/fs/readers/MultilineFS10AirportReader.java
+++ flightsim-jobs/load-airports-file-job/src/main/java/com/crimsonpig/fs/readers/MultilineFS10AirportReader.java
@@ -6,20 +6,30 @@ import org.springframework.batch.item.ItemStreamReader;
 import org.springframework.batch.item.NonTransientResourceException;
 import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.FieldSetMapper;
 import org.springframework.batch.item.file.transform.FieldSet;
 
 import com.crimsonpig.fs.domain.airport.Airport;
+import com.crimsonpig.fs.domain.airport.ParkingSpot;
+import com.crimsonpig.fs.domain.airport.Runway;
 import com.crimsonpig.fs.mappers.AirportFieldSetMapper;
+import com.crimsonpig.fs.mappers.ParkingSpotFieldSetMapper;
+import com.crimsonpig.fs.mappers.RunwayFieldSetMapper;
 
 
 public class MultilineFS10AirportReader implements ItemStreamReader<Airport> {
 
 	private ItemStreamReader<FieldSet> delegate;
+	
+	private FieldSetMapper<Airport> airportFieldSetMapper;
+	private FieldSetMapper<Runway> runwayFieldSetMapper;	
+	private FieldSetMapper<ParkingSpot> parkingFieldSetMapper;
 
 	public MultilineFS10AirportReader(ItemStreamReader<FieldSet> delegate){
 		this.delegate = delegate;
+		this.airportFieldSetMapper = new AirportFieldSetMapper();
+		this.runwayFieldSetMapper = new RunwayFieldSetMapper();
+		this.parkingFieldSetMapper = new ParkingSpotFieldSetMapper();
 	}
 	
 	@Override
@@ -49,7 +59,7 @@ public class MultilineFS10AirportReader implements ItemStreamReader<Airport> {
 			
 			if(!inRunwaysAndParking){
 				if(line.getFieldCount() > 1){
-					airport = airportFieldSetMapper().mapFieldSet(line);
+					airport = airportFieldSetMapper.mapFieldSet(line);
 				} else if(line.getFieldCount() == 1 && "{".equals(line.readString(0))){
 					inRunwaysAndParking = true;
 				}
@@ -58,7 +68,13 @@ public class MultilineFS10AirportReader implements ItemStreamReader<Airport> {
 					inRunwaysAndParking = false;
 					break;
 				} else if(line.getFieldCount() > 1){
-					
+					if("PARKING".equals(line.readString(0))){
+						ParkingSpot parking = parkingFieldSetMapper.mapFieldSet(line);
+						airport.addParkingSpot(parking);
+					} else if("RUNWAY".equals(line.readString(0))){
+						Runway runway = runwayFieldSetMapper.mapFieldSet(line);
+						airport.addRunway(runway);
+					}
 				}
 			}
 		}
@@ -66,9 +82,6 @@ public class MultilineFS10AirportReader implements ItemStreamReader<Airport> {
 		return airport;
 	}
 
-	private FieldSetMapper<Airport> airportFieldSetMapper(){
-		AirportFieldSetMapper mapper = new AirportFieldSetMapper();
-		return mapper;
-	}
+
 
 }
