5df53580d5ea681bbe6da9bc56f77b998f6886eb
crimsonpig <keith.stabbert@yahoo.com>
2016-09-05 20:47:46 -0700

    Adding failing test for MultilineFS10AirportReader

--- flightsim-jobs/load-airports-file-job/src/test/java/com/crimsonpig/fs/readers/MultilineFS10AirportReaderTest.java
+++ flightsim-jobs/load-airports-file-job/src/test/java/com/crimsonpig/fs/readers/MultilineFS10AirportReaderTest.java
@@ -2,18 +2,21 @@ package com.crimsonpig.fs.readers;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.batch.item.ExecutionContext;
-import org.springframework.batch.item.NonTransientResourceException;
-import org.springframework.batch.item.ParseException;
-import org.springframework.batch.item.UnexpectedInputException;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.transform.FieldSet;
 import org.springframework.core.io.ClassPathResource;
 
 import com.crimsonpig.fs.domain.airport.Airport;
 import com.crimsonpig.fs.domain.airport.Airspace;
+import com.crimsonpig.fs.domain.airport.ParkingSpot;
+import com.crimsonpig.fs.domain.airport.ParkingType;
+import com.crimsonpig.fs.domain.airport.Runway;
+import com.crimsonpig.fs.domain.airport.Surface;
 import com.crimsonpig.fs.mappers.AirportLineMapper;
 
 public class MultilineFS10AirportReaderTest {
@@ -44,6 +47,12 @@ public class MultilineFS10AirportReaderTest {
 			fail();
 		}
 		assertNotNull(actual);
+		assertAirportHeader(actual);
+		assertRunways(actual.getRunways());
+		assertParkingSpots(actual.getParkingSpots());
+	}	
+
+	private void assertAirportHeader(Airport actual){
 		assertEquals("KMCI", actual.getIdentifier());
 		assertEquals(39.297605529, actual.getLatitudeDegrees(), 0.0000000001);
 		assertEquals(-94.713905454, actual.getLongitudeDegrees(), 0.0000000001);
@@ -51,7 +60,27 @@ public class MultilineFS10AirportReaderTest {
 		assertEquals(Airspace.B, actual.getAirspace());
 		assertEquals(6, actual.getApproachFrequencies());
 		assertEquals(0.70, actual.getTrafficScalar(), 0.01);
-		assertEquals("United States", actual.getCountry());		
+		assertEquals("United States", actual.getCountry());			
 	}
+	
+	private void assertRunways(List<Runway> runways) {
+		assertNotNull(runways);
+		assertEquals(3, runways.size());
+		Runway longest = runways.get(0);
+		assertEquals(10810, longest.getLength());
+		assertEquals(150, longest.getWidth());
+		assertEquals(Surface.HARD, longest.getSurface());
+		assertEquals("KMCI", longest.getAirportIdentifier());
+	}
+	
+	private void assertParkingSpots(List<ParkingSpot> parkingSpots) {
+		assertNotNull(parkingSpots);
+		assertEquals(43, parkingSpots.size());
+		ParkingSpot gate = parkingSpots.get(42);
+		assertEquals(23.0, gate.getRadius(), 0.001);
+		assertEquals(ParkingType.GATE, gate.getParkingType());
+		assertEquals("KMCI", gate.getAirportIdentifier());
+	}	
+
 	
 }
